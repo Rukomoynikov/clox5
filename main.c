ae3741ae3b854f1beb760555d5695ce0f147de36
Max Rukomoynikov <rukomoynikov@gmail.com>
2026-01-15 19:54:20 +0000

    VM machine based in Stack (Chapter 2)

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int arg_count, const char* argv[]) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
   int constantIndex = addConstant(&chunk, 1);
   writeChunk(&chunk, OP_CONSTANT, 100);
   writeChunk(&chunk, constantIndex, 100);
+  writeChunk(&chunk, OP_NEGATE, 100);
+  writeChunk(&chunk, OP_CONSTANT, 100);
+  writeChunk(&chunk, constantIndex, 100);
   writeChunk(&chunk, OP_RETURN, 100);
-  disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  // disassembleChunk(&chunk, "test chunk");
+  freeVM();
   freeChunk(&chunk);
 }
---
+++ vm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "common.h"
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+  Chunk * chunk;
+  uint8_t * ip;
+  Value stack[STACK_MAX];
+  Value * stackTop;
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM();
+InterpretResult interpret(Chunk * chunk);
+void freeVM();
+void push(Value value);
+Value pop();
