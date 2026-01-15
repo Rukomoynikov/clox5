759687a7bc30d1158d28cc5b94e7349818e13a92
Max Rukomoynikov <rukomoynikov@gmail.com>
2026-01-15 20:10:00 +0000

    VM machine binary operations (Chapter 2)

--- main.c
+++ main.c
@@ -12,6 +12,17 @@ int main(int arg_count, const char* argv[]) {
   writeChunk(&chunk, OP_NEGATE, 100);
   writeChunk(&chunk, OP_CONSTANT, 100);
   writeChunk(&chunk, constantIndex, 100);
+  constantIndex = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constantIndex, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constantIndex = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constantIndex, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
   writeChunk(&chunk, OP_RETURN, 100);
   interpret(&chunk);
   // disassembleChunk(&chunk, "test chunk");
--- vm.c
+++ vm.c
@@ -23,6 +23,12 @@ InterpretResult interpret(Chunk * chunk) {
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
   #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+  #define BINARY_OP(op) \
+    do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false)
 
   for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION
@@ -44,6 +50,10 @@ static InterpretResult run() {
       case OP_NEGATE:
         push(-pop());
         break;
+      case OP_ADD: BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE: BINARY_OP(/); break;
       case OP_CONSTANT:
         Value constant = READ_CONSTANT();
         push(constant);
@@ -52,6 +62,7 @@ static InterpretResult run() {
         break;
     }
   }
+  #undef BINARY_OP
   #undef READ_BYTE
   #undef READ_CONSTANT
 }
