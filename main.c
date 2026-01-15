9e8686181e4c97ade04044f90d2a85d8e6e1d12f
Max Rukomoynikov <rukomoynikov@gmail.com>
2026-01-15 20:45:30 +0000

    Repl and runfile introduction (Chapter 3)

--- main.c
+++ main.c
@@ -1,31 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+static void repl();
+static void runFile(char * filePath);
+static char * readFile(char * filePath);
+
 int main(int arg_count, const char* argv[]) {
   initVM();
-  Chunk chunk;
-  initChunk(&chunk);
-  int constantIndex = addConstant(&chunk, 1);
-  writeChunk(&chunk, OP_CONSTANT, 100);
-  writeChunk(&chunk, constantIndex, 100);
-  writeChunk(&chunk, OP_NEGATE, 100);
-  writeChunk(&chunk, OP_CONSTANT, 100);
-  writeChunk(&chunk, constantIndex, 100);
-  constantIndex = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constantIndex, 123);
 
-  writeChunk(&chunk, OP_ADD, 123);
+  repl();
 
-  constantIndex = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constantIndex, 123);
+  if (arg_count == 1) {
+    repl();
+  } else if (arg_count == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Unknown number of arguments\n");
+    exit(64);
+  }
 
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_RETURN, 100);
-  interpret(&chunk);
-  // disassembleChunk(&chunk, "test chunk");
   freeVM();
-  freeChunk(&chunk);
+  return 0;
+}
+
+static void repl() {
+  char line[1024];
+
+  for (;;) {
+    printf("> ");
+
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+
+    interpret(line);
+  }
+}
+
+static void runFile(char * filePath) {
+  char * source = readFile(filePath);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR) exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
+static char * readFile(char * filePath) {
+  FILE * file = fopen(filePath, "rb");
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char* buffer = (char*)malloc(fileSize + 1);
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  buffer[bytesRead] = '\0';
+
+  fclose(file);
+  return buffer;
 }
